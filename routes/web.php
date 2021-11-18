c79b14b546a145b28358d04a7326476eddc2919b
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-11-18 15:55:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,17 @@ Route::post('greetings',"ViewController@resultGreetings");
 
 Route::get('ets',"ViewController@formDataBarang");
 Route::get('palinphp',"ViewController@palindromecheckerPhp");
+
+Route::get('/pegawai','PegawaiController@index');
+Route::get('/pegawai/tambah','PegawaiController@tambah');
+
+Route::post('/pegawai/store','PegawaiController@store');
+Route::get('/pegawai/edit/{id}','PegawaiController@edit');
+
+Route::post('/pegawai/update','PegawaiController@update');
+Route::get('/pegawai/hapus/{id}','PegawaiController@hapus');
+
+
+
+
+
