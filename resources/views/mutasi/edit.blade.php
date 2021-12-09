ca73ac7dfcc27d18f0ef5990b160e0a477fe61c0
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-09 12:03:51 +0700

    Update edit.blade.php

--- resources/views/mutasi/edit.blade.php
+++ resources/views/mutasi/edit.blade.php
@@ -9,6 +9,11 @@
 <head>
 	<title>Edit</title>
 </head>
+<style>
+    .form-group{
+        margin-right : 100px;
+    }
+</style>
 <body>
 	<h3>Edit Data Mutasi</h3>
 
@@ -16,18 +21,51 @@
 
 	<br/>
 	<br/>
-
+    <div class="container">
 	@foreach($mutasi as $m)
 	<form action="/mutasi/update" method="post">
 		{{ csrf_field() }}
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="ID">ID:</label>
+            <div class="col-sm-10">
+                <input type="number" name="ID" value="{{ $m->ID}}" class="form-control" required="required"> <br/>
+            </div>
+        </div>
+        <div class="form-group"><br/>
+            <label class="control-label col-sm-2" for="IDPegawai">IDPegawai :</label>
+            <div class="col-sm-10">
+                <input type="number" name="IDPegawai" value="{{ $m->IDPegawai }}" class="form-control" required="required"> <br/>
+            </div>
+        </div>
+        <div class="form-group"><br/>
+            <label class="control-label col-sm-2" for="Departemen">Departemen:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="Departemen" value="{{ $m->Departemen }}" required="required"> <br/>
+            </div>
+        </div>
+        <div class="form-group"><br/>
+            <label class="control-label col-sm-2" for="Subdepartemen">Subdepartemen:</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" name="SubDepartemen" value="{{ $m->SubDepartemen }}" required="required"> <br/>
+
+            </div>
+        </div>
+        <div class="form-group"><br/>
+            <label class="control-label col-sm-2" for="MulaiBertugas">MulaiBertugas:</label>
+            <div class="col-sm-10">
+                <input type="datetime-local" class="form-control" name="MulaiBertugas" value="{{ $m->MulaiBertugas }}" required="required"> <br/>
+
+            </div>
+        </div>
+        <div class="form-group"><br/>
+            <div class="col-sm-offset-2 col-sm-10">
+        <input type="submit" class="form-control" value="Simpan Data">
+    </div>
+</div>
 		<input type="hidden" name="id" value="{{ $m->ID }}"> <br/>
-		ID <input type="number" required="required" name="ID" value="{{ $m->ID}}"> <br/>
-		IDPegawai <input type="number" required="required" name="IDPegawai" value="{{ $m->IDPegawai }}"> <br/>
-		Departemen <input type="text" required="required" name="Departemen" value="{{ $m->Departemen }}"> <br/>
-		SubDepartemen <input type="text" required="required" name="SubDepartemen" value="{{ $m->SubDepartemen }}"><br/>
-        MulaiBertugas <input type="datetime-local" required="required" name="MulaiBertugas" value="{{ $m->MulaiBertugas }}"><br/>
-		<input type="submit" value="Simpan Data">
+
 	</form>
+</div>
 	@endforeach
 @endsection
 
