afda5775972f86971fe0ad0e843542050dc25878
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-14 16:49:44 +0700

    Update PegawaiController.php

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -11,7 +11,8 @@ class PegawaiController extends Controller
     public function index()
     {
         // DB::table('')->get();
-        $pegawai = DB::table('pegawai')->get();
+        $pegawai = DB::table('pegawai')->paginate(5);
+
         return view('pegawai.index',['pegawai' => $pegawai]);
     }
     public function tambah()
@@ -68,5 +69,26 @@ public function hapus($id)
 	// alihkan halaman ke halaman pegawai
 	return redirect('/pegawai');
 }
+public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$pegawai = DB::table('pegawai')
+		->where('pegawai_nama','like',"%".$cari."%")
+		->paginate();
+
+    		// mengirim data pegawai ke view index
+		return view('pegawai.index',['pegawai' => $pegawai]);
+
+	}
+    public function detail($id)
+    {
+        // mengambil data pegawai berdasarkan id yang dipilih
+        $pegawai = DB::table('pegawai')->where('pegawai_id', $id)->get();
+        // passing data pegawai yang didapat ke view edit.blade.php
+        return view('pegawai.detail', ['pegawai' => $pegawai]);
+    }
 
 }
