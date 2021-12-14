49a3b39098a819919cbd5c120313d54d993f670e
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-14 16:56:31 +0700

    Update index.blade.php

--- resources/views/pegawai/index.blade.php
+++ resources/views/pegawai/index.blade.php
@@ -19,40 +19,38 @@
         align-content : center;
     }
 </style>
+<style type="text/css">
+		.pagination li{
+			float: left;
+			list-style-type: none;
+			margin:5px;
+		}
+	</style>
 
 <body>
-
- 
-
-	<h2>www.malasngoding.com</h2>
-
 	<h3>Data Pegawai</h3>
-
- 
-
-	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
-
-
-
-	<br/>
-
-	<br/>
-
-     <div class="container">
+    <div class="container">
+        <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+        <div class="container" align="center">
+        <form action="/pegawai/cari" method="GET">
+		<input type="text" class="form-control" name="cari" placeholder="Cari Pegawai berdasarkan nama" value="{{ old('cari') }}">
+		<input type="submit" class="btn btn-default" value="CARI">
+	</form>
+</div>
         <table class="table table-bordered">
             <thead>
 
 		<tr>
 
-			<th>Nama</th>
+			<th class="isi-tabel" >Nama</th>
 
-			<th>Jabatan</th>
+			<th class="isi-tabel" >Jabatan</th>
 
-			<th>Umur</th>
+			<th class="isi-tabel" >Umur</th>
 
-			<th>Alamat</th>
+			<th class="isi-tabel" >Alamat</th>
 
-			<th>Opsi</th>
+			<th class="isi-tabel" >Opsi</th>
 
 		</tr>
 
@@ -60,20 +58,19 @@
 
 		<tr>
 
-			<td>{{ $p->pegawai_nama }}</td>
+			<td class="isi-tabel">{{ $p->pegawai_nama }}</td>
 
-			<td>{{ $p->pegawai_jabatan }}</td>
+			<td class="isi-tabel">{{ $p->pegawai_jabatan }}</td>
 
-			<td>{{ $p->pegawai_umur }}</td>
+			<td class="isi-tabel">{{ $p->pegawai_umur }}</td>
 
-			<td>{{ $p->pegawai_alamat }}</td>
+			<td class="isi-tabel">{{ $p->pegawai_alamat }}</td>
 
 			<td>
-
-				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
-
+                <a href="/pegawai/view/{{ $p->pegawai_id }}">View Detail</a>
 				|
-
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+				|
 				<a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
 
 			</td>
@@ -83,10 +80,13 @@
 		@endforeach
     </tbody>
 </table>
+<br/>
+	Halaman : {{ $pegawai->currentPage() }} <br/>
+	Jumlah Data : {{ $pegawai->total() }} <br/>
+	Data Per Halaman : {{ $pegawai->perPage() }} <br/>
+{{ $pegawai->links() }}
 </div>
- 
-
- @endsection
+@endsection
 
 </body>
 
