102be700ff283e966728059f2ef1a49c472ee43b
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-14 16:51:19 +0700

    Update indexabsen.blade.php

--- resources/views/absen/indexabsen.blade.php
+++ resources/views/absen/indexabsen.blade.php
@@ -20,31 +20,33 @@
 </style>
 
 <body>
-
 	<h2>Daftar Absensi Pegawai</h2>
-
-
-	<a href="/absen/add"> + Tambah Absensi</a>
-
-	<br/>
-	<br/>
     <div class="container">
+        <a href="/absen/add"> + Tambah Absensi</a>
+        <div class="container" align="center">
+            <form action="/absen/cari" method="GET">
+            <input type="text" class="form-control" name="cari" placeholder="Cari Absensi berdasarkan nama" value="{{ old('cari') }}">
+            <input type="submit" class="btn btn-default" value="CARI">
+        </form>
+    </div>
 	<table class="table table-bordered">
         <thead>
 		<tr>
 
-			<th>IDPegawai</th>
-			<th>Tanggal</th>
-			<th>Status</th>
-			<th>Opsi</th>
+			<th class="isi-tabel">IDPegawai</th>
+			<th class="isi-tabel">Tanggal</th>
+			<th class="isi-tabel">Status</th>
+			<th class="isi-tabel">Opsi</th>
 		</tr>
 		@foreach($absen as $a)
 		<tr>
 
-			<td>{{ $a->IDPegawai }}</td>
-			<td>{{ $a->Tanggal }}</td>
-			<td>{{ $a->Status }}</td>
+			<td class="isi-tabel">{{ $a->IDPegawai }}</td>
+			<td class="isi-tabel">{{ $a->Tanggal }}</td>
+			<td class="isi-tabel">{{ $a->Status }}</td>
 			<td>
+                <a href="/absen/view/{{ $a->ID }}">View Detail</a>
+				|
 				<a href="/absen/edit/{{ $a->ID }}">Edit Absensi</a>
 				|
 				<a href="/absen/hapus/{{ $a->ID }}">Delete Absensi</a>
@@ -53,13 +55,21 @@
 		@endforeach
     </tbody>
 	</table>
+    <br>
+Halaman : {{ $absen->currentPage() }} <br/>
+	Jumlah Data : {{ $absen->total() }} <br/>
+	Data Per Halaman : {{ $absen->perPage() }} <br/>
+    {{ $absen->links() }}
 </div>
     <p>
         Keterangan Status: <br>
+        H : Hadir <br>
         I : Izin <br>
         S : Sakit <br>
         A : Alpha <br>
         </p>
+
+
 @endsection
 </body>
 </html>
