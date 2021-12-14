e714ef587ca0e1d5c143327ecf88d38d2c97fcd4
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-14 16:48:15 +0700

    Update AbsenController.php

--- app/Http/Controllers/AbsenController.php
+++ app/Http/Controllers/AbsenController.php
@@ -11,18 +11,22 @@ class AbsenController extends Controller
     public function indexabsen()
     {
         // mengambil data dari table absen
-        $absen = DB::table('absen')->get();
-
+        // $absen = DB::table('absen')->paginate(5);
+        $absen = DB::table('absen')
+        ->join('pegawai','absen.IDPegawai','=','pegawai.pegawai_id')
+        ->select('absen.*','pegawai.pegawai_nama')
+        ->paginate();
         // mengirim data absen ke view indexabsen
         return view('absen.indexabsen', ['absen' => $absen]);
     }
     // method untuk menampilkan view form tambah absen
     public function add()
     {
-        // mengambil data dari table pegawai
-        $pegawai = DB::table('pegawai')->orderBy('pegawai_nama', 'asc')->get(); //defaultnya urut Primary Key
+        // $pegawai = DB::table('pegawai')->orderBy('pegawai_nama', 'asc')->get(); //defaultnya urut Primary Key
         // memanggil view add
-        return view('absen.add', ['pegawai' => $pegawai]);
+
+        return view('absen.add');
+        // , ['pegawai' => $pegawai]);
     }
     // method untuk insert data ke table absen
     public function store(Request $request)
@@ -30,9 +34,9 @@ class AbsenController extends Controller
         // insert data ke table absen
         DB::table('absen')->insert([
 
-            'IDPegawai' => $request->idpegawai,
-            'Tanggal' => $request->tanggal,
-            'Status' => $request->status
+            'IDPegawai' => $request->IDPegawai,
+            'Tanggal' => $request->Tanggal,
+            'Status' => $request->Status
         ]);
         // alihkan halaman ke halaman absen
         return redirect('/absen');
@@ -46,10 +50,10 @@ class AbsenController extends Controller
         // mengambil data dari table pegawai
         $pegawai = DB::table('pegawai')->orderBy('pegawai_nama', 'asc')->get(); //defaultnya urut Primary Key
 
-      
+
 
         // passing data absen yang didapat ke view update.blade.php
-        return view('absen.edit', ['absen' => $absen,'pegawai' => $pegawai , 'judul' => $judul]);
+        return view('absen.edit', ['absen' => $absen,'pegawai' => $pegawai ]);
     }
     // update data absen
     public function update(Request $request)
@@ -72,5 +76,26 @@ class AbsenController extends Controller
 
         // alihkan halaman ke halaman absen
         return redirect('/absen');
+    }
+    public function cari(Request $request)
+	{
+		// menangkap data pencarian
+		$cari = $request->cari;
+
+    		// mengambil data dari table pegawai sesuai pencarian data
+		$absen = DB::table('absen')
+		->where('IDPegawai','like',"%".$cari."%")
+		->paginate();
+
+    		// mengirim data pegawai ke view index
+		return view('absen.indexabsen',['absen' => $absen]);
+
+	}
+    public function detail($id)
+    {
+        // mengambil data pegawai berdasarkan id yang dipilih
+        $absen = DB::table('absen')->where('ID', $id)->get();
+        // passing data pegawai yang didapat ke view edit.blade.php
+        return view('absen.detail', ['absen' => $absen]);
     }
 }
