598fd8d02bf9fdeafc25d446d3dffba5ab1626c1
Aqmarina Nindya <aqmarinanindy20@gmail.com>
2021-12-14 16:51:53 +0700

    Create detail.blade.php

---
+++ resources/views/lipstick/detail.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.bahagia')
+
+@section('title', 'Detail Tabel Lipstick')
+@section('judulhalaman', 'Detail Data Lipstick')
+
+@section('konten')
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Detail Data Lipstick</title>
+</head>
+<style>
+    /* .form-group{
+        margin-right : 100px;
+    } */
+
+</style>
+<body>
+    @foreach($lipstick as $a)
+    <div class="container">
+
+        <h3>Detail Data Lipstick</h3><br>
+        <div class="form-group row">
+            <label class="col-sm-2" for="kodelipstick"> kodelipstick </label>
+            <div class="col-sm-10" > : {{ $a->kodelipstick}}</div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2" for="merklipstick"> merklipstick </label>
+            <div class="col-sm-10" > : {{ $a->merklipstick}}</div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2" for="stocklipstick"> stocklipstick </label>
+            <div class="col-sm-10" > : {{ $a->stocklipstick}}</div>
+        </div>
+        <div class="form-group row">
+            <label class="col-sm-2" for="tersedia"> tersedia </label>
+            <div class="col-sm-10" > : {{ $a->tersedia}}</div>
+        </div>
+</div>
+@endforeach
+<br>
+<a href="/lipstick"> Kembali</a>
+@endsection
+</body></html>
